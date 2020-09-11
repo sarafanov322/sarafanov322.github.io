358865663cf943139bce1b4a269176031897663a
sarafanov322 <70897696+sarafanov322@users.noreply.github.com>
2020-09-11 04:11:16 +0300

    Add files via upload

---
+++ precache-manifest.e0c061264e599dc7898a63585266428a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18ccbc4ae3519e63a6f526ae1010bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb94cd5f62b7a3a73832",
+    "url": "/static/css/main.567708f2.chunk.css"
+  },
+  {
+    "revision": "150f78816869cab2a547",
+    "url": "/static/js/2.87d4282f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.87d4282f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb94cd5f62b7a3a73832",
+    "url": "/static/js/main.692a0d4a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
